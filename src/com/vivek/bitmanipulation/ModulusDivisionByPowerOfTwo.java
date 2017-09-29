8b132e17284d084d36d61e8b7910bf4c156da3fd
Vivek Jaiswal <vivekjaiswal@live.in>
2017-09-30 02:10:38 +0530

    added problem in bit manipulation category

---
+++ src/com/vivek/bitmanipulation/ModulusDivisionByPowerOfTwo.java
@@ -0,0 +1,23 @@
+package com.vivek.bitmanipulation;
+
+/**
+ * Created by VJaiswal on 26/09/17.
+ */
+public class ModulusDivisionByPowerOfTwo {
+
+    public static void main(String[] args) {
+        // Compute modulus division by 1 << s without a division operator
+        int n = 34; // numerator
+        int s = 3; // shift constant
+        // denominator
+        int d = 1 << s; // this will be 1, 2, 4, 8, 16, 32 ...
+
+        int result = n & (d - 1);
+
+        System.out.println("Input: " + n);
+        System.out.println("Output: " + result);
+
+
+    }
+
+}
