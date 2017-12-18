b5220ac0df2e9e66a89433d9bdc4e3c90d0d7c22
Vivek Jaiswal <vivekjaiswal@live.in>
2017-12-19 01:01:32 +0530

    added problem of floor ceil node in bast

---
+++ src/com/vivek/tree/bst/FloorCeilNode.java
@@ -0,0 +1,45 @@
+package com.vivek.tree.bst;
+
+import com.vivek.tree.Node;
+
+/**
+ * Created by VJaiswal on 30/11/17.
+ */
+public class FloorCeilNode {
+
+    static int floor(Node node, int key) {
+        if (node == null)
+            return -1;
+
+        if (node.data == key) {
+            return node.data;
+        }
+
+        if (node.data > key) {
+            return floor(node.left, key);
+        }
+
+        int floor = floor(node.right, key);
+        return floor <= key ? floor : node.data;
+    }
+
+    static int ceil(Node node, int key) {
+
+        if (node == null)
+            return -1;
+
+        if (node.data == key) {
+            return node.data;
+        }
+
+        // If root's key is smaller, ceil must be in right subtree
+        if (node.data < key) {
+            return ceil(node.right, key);
+        }
+
+        // Else, either left subtree or root has the ceil value
+        int ceil = ceil(node.left, key);
+        return ceil >= key ? ceil : node.data;
+    }
+
+}
