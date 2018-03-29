c79f8f99b18ea5558e765296d21ea770ae2dc627
Vivek Jaiswal <vivekjaiswal@live.in>
2018-03-29 07:34:13 +0530

    added recursive reversal of linked list

--- src/com/vivek/linkedlist/ReverseLinkedList.java
+++ src/com/vivek/linkedlist/ReverseLinkedList.java
@@ -1,8 +1,9 @@
 package com.vivek.linkedlist;
 
 public class ReverseLinkedList {
-	
-	Node reverse(Node head) {
+
+	// Iterative solution for reversal
+	Node iterativeReverse(Node head) {
 	    Node prev = null;
 	    Node current = head;
 	    Node next = null;
@@ -16,6 +17,36 @@ public class ReverseLinkedList {
 	    }
 	    
 	    return prev;
+	}
+
+	// Recursive solution for reversal
+	Node recursiveReverse(Node head) {
+		/**
+		 What is the reverse of null (the empty list)? null.
+		 What is the reverse of a one element list? the element.
+		 What is the reverse of an n element list? the reverse of the second element on followed by the first element.
+		 */
+
+		if (head == null) { // Null List
+			return null;
+		}
+
+		if (head.next == null) {	// Single element list
+			return head;
+		}
+
+		Node secondElem = head.next;
+
+		// need to unlink list from the rest or you will get a cycle
+		head.next = null;
+
+		// then we reverese everything from the second element on
+		Node reverseRest = recursiveReverse(secondElem);
+
+		// then we join the two lists
+		secondElem.next = head;
+
+		return reverseRest;
 	}
 
 }
