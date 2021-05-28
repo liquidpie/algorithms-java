e80c3924d73aad2325e5dcf79fc62875c66af263
liquidpie <vivekjaiswal@live.in>
2021-05-28 17:51:23 +0530

    Binary Search and Remove Duplicates

--- src/com/vivek/array/ArrayMisc.java
+++ src/com/vivek/array/ArrayMisc.java
@@ -102,4 +102,20 @@ public class ArrayMisc {
         }
     }
 
+    /**
+     * Returns the size of the new array after removing duplicates
+     */
+    static int removeDuplicates(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return -1;
+
+        int j = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1])
+                arr[j++] = arr[i];
+        }
+        arr[j++] = arr[arr.length - 1];
+        return j;
+    }
+
 }
---
+++ src/com/vivek/search/BinarySearch.java
@@ -0,0 +1,34 @@
+package com.vivek.search;
+
+public class BinarySearch {
+
+    /**
+     * Returns index of key if it is present in arr[low..high], else return -1
+     */
+    public static int iterative(int[] arr, int low, int high, int key) {
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key)
+                return mid;
+            else if (key < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    public static int recursive(int[] arr, int low, int high, int key) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key)
+                return mid;
+            else if (key < arr[mid])
+                return recursive(arr, low, mid - 1, key);
+            else
+                return recursive(arr, mid + 1, high, key);
+        }
+        return -1;
+    }
+
+}
