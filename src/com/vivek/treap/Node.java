fe0e55b118247875a0e8a53e396e86f5eeadeb3b
liquidpie <vivekjaiswal@live.in>
2021-06-01 19:49:04 +0530

    Treap node

---
+++ src/com/vivek/treap/Node.java
@@ -0,0 +1,18 @@
+package com.vivek.treap;
+
+import java.util.Random;
+
+public class Node {
+
+    private static final Random random = new Random();
+
+    public int key;
+    public int priority;
+    public Node left, right;
+
+    public Node(int key) {
+        this.key = key;
+        this.priority = random.nextInt(100);
+    }
+
+}
