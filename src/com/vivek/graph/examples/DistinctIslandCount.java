72b31d2b3b4d5639f6fe3dc8d9a8a598dac2e666
liquidpie <vivekjaiswal@live.in>
2023-07-12 14:20:05 +0530

    refactor

--- src/com/vivek/graph/examples/DistinctIslandCount.java
+++ src/com/vivek/graph/examples/DistinctIslandCount.java
@@ -6,16 +6,24 @@ import java.util.Set;
 /**
  * Number of Distinct Islands
  *
- * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+ * You may assume all four edges of the grid are surrounded by water.
  *
- * Count the number of distinct islands. An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+ * Count the number of distinct islands.
+ *
+ * An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
  *
  * Solution:
- * At the beginning, we need to find every island, which we can do using a straightforward depth-first search. The hard part is deciding whether two islands are the same.
+ * At the beginning, we need to find every island, which we can do using a straightforward depth-first search.
+ * The hard part is deciding whether two islands are the same.
  *
- * Since two islands are the same if one can be translated to match another, let's translate every island so the top-left corner is (0, 0) For example, if an island is made from squares [(2, 3), (2, 4), (3, 4)], we can think of this shape as [(0, 0), (0, 1), (1, 1)] when anchored at the top-left corner.
+ * Since two islands are the same if one can be translated to match another,
+ * let's translate every island so the top-left corner is (0, 0) For example,
+ * if an island is made from squares [(2, 3), (2, 4), (3, 4)], we can think of this shape as [(0, 0), (0, 1), (1, 1)]
+ * when anchored at the top-left corner.
  *
- * From there, we only need to check how many unique shapes there ignoring permutations (so [(0, 0), (0, 1)] is the same as [(0, 1), (1, 0)]). We use sets directly as we have showcased below, but we could have also sorted each list and put those sorted lists in our set structure shapes.
+ * From there, we only need to check how many unique shapes there ignoring permutations (so [(0, 0), (0, 1)] is the same as [(0, 1), (1, 0)]).
+ * We use sets directly as we have showcased below, but we could have also sorted each list and put those sorted lists in our set structure shapes.
  */
 public class DistinctIslandCount {
 
