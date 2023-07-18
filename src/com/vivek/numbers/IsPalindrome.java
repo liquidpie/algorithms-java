65ca4bd841b857b7765e306db9e3e2645e044e09
liquidpie <vivekjaiswal@live.in>
2023-07-18 11:02:46 +0530

    new problem

---
+++ src/com/vivek/numbers/IsPalindrome.java
@@ -0,0 +1,47 @@
+package com.vivek.numbers;
+
+/**
+ * Palindrome Number
+ *
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ *
+ * Example 1:
+ *
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ *
+ * Example 2:
+ *
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ *
+ * Example 3:
+ *
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ *
+ * https://leetcode.com/problems/palindrome-number
+ */
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+    }
+
+    static boolean isPalindrome(int x) {
+        int revX = 0;
+        int temp = x;
+        // 453
+        while (temp > 0) {
+            revX = (temp % 10) + 10 * revX;
+            temp /= 10;
+        }
+
+        return x == revX;
+    }
+
+}
