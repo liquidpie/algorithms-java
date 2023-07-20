ae33ab23d6fe882ed91e559181b69c51305ec3b7
liquidpie <vivekjaiswal@live.in>
2023-07-21 00:04:56 +0530

    new problem

---
+++ src/com/vivek/bitmanipulation/SingleNumber.java
@@ -0,0 +1,42 @@
+package com.vivek.bitmanipulation;
+
+/**
+ * Single Number
+ *
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: nums = [4,1,2,1,2]
+ * Output: 4
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: 1
+ *
+ * https://leetcode.com/problems/single-number
+ */
+public class SingleNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+
+    static int singleNumber(int[] nums) {
+        int n = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            n = n ^ nums[i];
+        }
+
+        return n;
+    }
+
+}
