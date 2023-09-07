bc6e5df5792e66308aafcb3b6adb6f4d63f5e9ef
liquidpie <vivekjaiswal@live.in>
2023-09-07 19:30:33 +0530

    refactor

--- src/com/vivek/array/RemoveDuplicates.java
+++ src/com/vivek/array/RemoveDuplicates.java
@@ -1,21 +1,35 @@
 package com.vivek.array;
 
 /**
- * Returns the size of the new array after removing duplicates
+ * Remove Duplicates
+ *
+ * Given an array of sorted numbers, remove all duplicates from it.
+ * You should not use any extra space; after removing the duplicates in-place return the new length of the array.
+ *
+ * Example 1
+ * Input: [2, 3, 3, 3, 6, 9, 9]
+ * Output: 4
+ * Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
  */
 public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 3, 3, 6, 9, 9};
+        System.out.println(removeDuplicates(nums));
+    }
 
     static int removeDuplicates(int[] arr) {
         if (arr == null || arr.length == 0)
             return -1;
 
-        int j = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] != arr[i + 1])
-                arr[j++] = arr[i];
+        int nextNonDuplicate = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[nextNonDuplicate - 1] != arr[i]) {
+                arr[nextNonDuplicate] = arr[i];
+                nextNonDuplicate++;
+            }
         }
-        arr[j++] = arr[arr.length - 1];
-        return j;
+        return nextNonDuplicate;
     }
 
 }
