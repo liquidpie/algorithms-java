35d1ad645cd4b0fb5c343f282138db6fb9ac19c5
liquidpie <vivekjaiswal@live.in>
2024-02-04 17:44:49 +0530

    add comment

--- src/com/vivek/array/MergeTwoSortedArraysInOneArray.java
+++ src/com/vivek/array/MergeTwoSortedArraysInOneArray.java
@@ -51,8 +51,8 @@ public class MergeTwoSortedArraysInOneArray {
     static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
-
         int k = m + n - 1;
+        // compare and move elements to end of list
         while (i >= 0 && j >= 0) {
             if (nums1[i] >= nums2[j]) {
                 nums1[k] = nums1[i];
