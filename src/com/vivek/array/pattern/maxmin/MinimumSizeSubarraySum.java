1cefdeca78c324c51d0aad2092bafa90233f851c
liquidpie <vivekjaiswal@live.in>
2024-10-24 01:10:31 +0530

    two pointers problem

--- src/com/vivek/array/pattern/maxmin/MinimumSizeSubarraySum.java
+++ src/com/vivek/array/pattern/maxmin/MinimumSizeSubarraySum.java
@@ -23,6 +23,16 @@ package com.vivek.array.pattern.maxmin;
  * Output: 0
  *
  * https://leetcode.com/problems/minimum-size-subarray-sum
+ *
+ * Grokking the Coding Interview
+ * Pattern: Sliding Window
+ *
+ * Time Complexity:
+ * The time complexity of the above algorithm will be O(N).
+ * The outer for loop runs for all elements and the inner while loop processes each element only once,
+ * therefore the time complexity of the algorithm will be O(N + N ) which is asymptotically equivalent to O(N).
+ *
+ * Space Complexity: O(1)
  */
 public class MinimumSizeSubarraySum {
 
