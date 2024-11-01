b02cfa35e2715c51fe7a1c10cdf82329cd02e17a
liquidpie <vivekjaiswal@live.in>
2024-11-01 21:16:19 +0530

    tree bfs

--- src/com/vivek/tree/Traversal.java
+++ src/com/vivek/tree/Traversal.java
@@ -138,6 +138,23 @@ public class Traversal {
         }
     }
 
+    /**
+     * Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e.,
+     * the lowest level comes first.
+     * You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+     *
+     * Example 1:
+     * Reverse Level Order Traversal:
+     * [[4,5,6,7],
+     * [2,3],
+     * [1]]
+     *
+     * Example 2:
+     * Reverse Level Order Traversal:
+     * [[9,10,5],
+     * [7,1],
+     * [12]]
+     */
     public static void reverseLevelOrder(Node root) {
         List<List<Integer>> result = new ArrayList<>();
         Queue<Node> queue = new LinkedList<>();
