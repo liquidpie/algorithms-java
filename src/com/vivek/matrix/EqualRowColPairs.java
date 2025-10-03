515fc060a38b3fcff00f69d6252fe6fd6d8c00f2
liquidpie <vivekjaiswal@live.in>
2025-10-04 00:58:24 +0530

    new problem

---
+++ src/com/vivek/matrix/EqualRowColPairs.java
@@ -0,0 +1,59 @@
+package com.vivek.matrix;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2352. Equal Row and Column Pairs
+ *
+ * Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+ *
+ * A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+ *
+ * Example 1:
+ *
+ * Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+ * Output: 1
+ * Explanation: There is 1 equal row and column pair:
+ * - (Row 2, Column 1): [2,7,7]
+ *
+ * Example 2:
+ *
+ * Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+ * Output: 3
+ * Explanation: There are 3 equal row and column pairs:
+ * - (Row 0, Column 0): [3,1,2,2]
+ * - (Row 2, Column 2): [2,4,2,2]
+ * - (Row 3, Column 2): [2,4,2,2]
+ *
+ * Reference:
+ * https://leetcode.com/problems/equal-row-and-column-pairs
+ */
+public class EqualRowColPairs {
+
+    public int equalPairs(int[][] grid) {
+        Map<String, Integer> rows = new HashMap<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < grid[i].length; j++) {
+                builder.append(grid[i][j]).append("_");
+            }
+            String code = builder.toString();
+            rows.put(code, rows.getOrDefault(code, 0) + 1);
+        }
+
+        int count = 0;
+        for (int j = 0; j < grid[0].length; j++) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < grid.length; i++) {
+                builder.append(grid[i][j]).append("_");
+            }
+            String code = builder.toString();
+            count += rows.getOrDefault(code, 0);
+        }
+
+        return count;
+    }
+
+}
