a6c2eeba9486a94d15b5cc412c3a74fcc49f9e95
liquidpie <vivekjaiswal@live.in>
2025-11-01 00:57:11 +0530

    new problem

---
+++ src/com/vivek/tree/pattern/dfs/LongestZigZagPath.java
@@ -0,0 +1,81 @@
+package com.vivek.tree.pattern.dfs;
+
+import com.vivek.tree.Node;
+
+/**
+ * 1372. Longest ZigZag Path in a Binary Tree
+ *
+ * You are given the root of a binary tree.
+ *
+ * A ZigZag path for a binary tree is defined as follow:
+ *
+ *     Choose any node in the binary tree and a direction (right or left).
+ *     If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
+ *     Change the direction from right to left or from left to right.
+ *     Repeat the second and third steps until you can't move in the tree.
+ *
+ * Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+ *
+ * Return the longest ZigZag path contained in that tree.
+ *
+ * Example 1:
+ *
+ * Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1]
+ * Output: 3
+ * Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
+ *
+ * Example 2:
+ *
+ * Input: root = [1,1,1,null,1,null,null,1,1,null,1]
+ * Output: 4
+ * Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
+ *
+ * Example 3:
+ *
+ * Input: root = [1]
+ * Output: 0
+ *
+ * Reference:
+ * https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree
+ */
+public class LongestZigZagPath {
+
+    private int max = 0;
+
+    public int longestZigZag(Node root) {
+        dfs(root, true, 0); // left turn
+        dfs(root, false, 0); // right turn
+        return max;
+    }
+
+    private void dfs(Node node, boolean isLeft, int streak) {
+        if (node == null)
+            return;
+
+        max = Math.max(max, streak);
+
+        if (node.left != null) {
+            int next = !isLeft ? streak + 1 : 1;
+            dfs(node.left, true, next);
+        }
+
+        if (node.right != null) {
+            int next = isLeft ? streak + 1 : 1;
+            dfs(node.right, false, next);
+        }
+    }
+
+    public static void main(String[] args) {
+        LongestZigZagPath helper = new LongestZigZagPath();
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.left.right.left = new Node(5);
+        root.left.right.right = new Node(6);
+        root.left.right.left.right = new Node(7);
+
+        System.out.println(helper.longestZigZag(root));
+    }
+
+}
